d7ea1d3e6bb1fb8a1249f7577b133f299b4ccf63
petfaynder <akartolga0@gmail.com>
2025-11-18 20:19:41 +0300

    Feat: Force HTTPS in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Schema; // Import Schema facade
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Schema::defaultStringLength(191); // Set default string length
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
